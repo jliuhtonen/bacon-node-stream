5a32ffd73665e74acd2cfa9c95f96ba14d9a62ff
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2017-09-19 20:29:44 +0300

    Fix test promise handling, add a new test

--- test/nodeToBacon.ts
+++ test/nodeToBacon.ts
@@ -10,16 +10,66 @@ describe('Converting node streams to Bacon streams', function() {
     const testStream = new PassThrough({ objectMode: true })
     const testData = [5, 3, 6, 8, 9]
 
-    const baconStream = readableToBacon(testStream) as Bacon.EventStream<any, number>
+    const baconStream = readableToBacon(testStream, {objectMode: true}) as Bacon.EventStream<any, number>
+    const testPromise = baconStreamAsArray(baconStream)
+      .tap(result => {
+        console.log('tap')
+        assert.deepEqual(result, testData)
+      })
 
     testData.forEach(v => {
       testStream.write(v)
     })
 
-    const initialResult: number[] = []
-    Bluebird.resolve(baconStream.fold(initialResult, (arr, v) => arr.concat(v as number)).toPromise())
+    testStream.end()
+
+    return testPromise
+  })
+
+  it('should include string events in the Bacon stream from the node stream', () => {
+    const testStream = new PassThrough()
+    const testData = ['hey', 'ho', 'let\'s go']
+
+    const baconStream = readableToBacon(testStream) as Bacon.EventStream<any, number>
+    const testPromise = baconStreamAsArray(baconStream)
       .tap(result => {
-        assert.deepEqual(result, testData)
+        assert.deepEqual(result.map(r => r.toString()), testData)
       })
+
+    testData.forEach(v => {
+      testStream.write(v)
+    })
+
+    testStream.end()
+
+    return testPromise
+  })
+
+  it('should include buffer events in the Bacon stream from the node stream', () => {
+    const testStream = new PassThrough()
+    const testData = [
+      Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72, 0x73]),
+      Buffer.from([0x61, 0x72, 0x65]),
+      Buffer.from([0x63, 0x6f, 0x6f, 0x6c])
+    ]
+
+    const baconStream = readableToBacon(testStream) as Bacon.EventStream<any, Buffer>
+    const testPromise = baconStreamAsArray(baconStream)
+      .tap(result => {
+        assert.deepEqual(result.map(r => r.toString()).join(' '), 'buffers are cool')
+      })
+
+    testData.forEach(v => {
+      testStream.write(v)
+    })
+
+    testStream.end()
+
+    return testPromise
   })
 })
+
+function baconStreamAsArray<T>(stream: Bacon.EventStream<any, T>): Bluebird<T[]> {
+  const initialResult: T[] = []
+  return Bluebird.resolve(stream.fold(initialResult, (arr, v) => arr.concat(v as T)).toPromise())
+}
