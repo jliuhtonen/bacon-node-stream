dc6cc1968a400de1d31ec316a40325918e456154
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2017-09-19 21:14:06 +0300

    Test that pausing readables from Bacon works

--- test/baconToNode.ts
+++ test/baconToNode.ts
@@ -23,4 +23,35 @@ describe('Converting Bacon streams to Node streams', function() {
       assert.deepEqual(results, testData)
     })
   })
+
+  it('should buffer while the stream is paused', function() {
+    this.timeout(20000)
+    const testData = ['hey', 'ho', 'let\'s go', 'ey', 'o', 'letsgo']
+    const baconStream = Bacon.sequentially(500, testData)
+    const nodeStream = baconToReadable(baconStream, { objectMode: true })
+
+    return new Bluebird((resolve, reject) => {
+      let shouldPauseNext = true
+      const results: string[] = []
+
+      nodeStream.on('data', (value: any) => {
+        results.push(value)
+        if (shouldPauseNext) {
+          nodeStream.pause()
+          shouldPauseNext = false
+          setTimeout(() => {
+            nodeStream.resume()
+          }, 3000)
+        } else {
+          shouldPauseNext = true
+        }
+      })
+
+      nodeStream.on('end', () => resolve(results))
+      nodeStream.on('close', () => resolve(results))
+      nodeStream.on('error', err => reject(err))
+    }).tap(results => {
+      assert.deepEqual(results, testData)
+    })
+  })
 })
